f47bfe7e0238cfedf09365055b2b867cd3369e23
Serhii Vandenvin <sevandenvin@gmail.com>
2022-08-30 15:43:36 +0300

    fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,15 +5,26 @@ const input = document.querySelector('input');
 const textarea = document.querySelector('textarea');
 
 const STORAGE_KEY = 'fedback-form-state';
+
 const formData = {
   email: '',
   message: '',
 };
 
+checkLocalStoradge();
+populateInputFromLocalStoradge();
+
 form.addEventListener('input', throttle(onInputChange, 500));
 form.addEventListener('submit', onFormSubmit);
 
-populateInputFromLocalStoradge();
+function checkLocalStoradge() {
+  if (localStorage.getItem(STORAGE_KEY) === null) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  }
+
+  formData.email = JSON.parse(localStorage.getItem(STORAGE_KEY)).email;
+  formData.message = JSON.parse(localStorage.getItem(STORAGE_KEY)).message;
+}
 
 function onInputChange(event) {
   formData[event.target.name] = event.target.value;
